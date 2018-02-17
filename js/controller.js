f60912e00f6cf671578bf68258cad2d82d7ffa10
GustavoDinizMonteiro <gustavo.monteiro@ccc.ufcg.edu.br>
2018-02-17 16:57:21 +0000

    Add form for basic baskets creation.

--- js/controller.js
+++ js/controller.js
@@ -16,7 +16,6 @@ app.controller('products', ($scope, $http) => {
 
     $scope.createProduct = () => {
         $http.post(products, $scope.product).then(res => {
-            console.log(res.data);
             $scope.products.push(res.data);
             delete $scope.product;
         });
@@ -27,6 +26,32 @@ app.controller('products', ($scope, $http) => {
             if(product.selected) {
                 $http.delete(products.concat('/', product.id)).then(res => $scope.products = $scope.products.filter(p => p.id != product.id));
             }
+        });
+    };
+});
+
+app.controller('basket', ($scope, $http) => {
+    const base = 'https://food10-api.herokuapp.com/';
+    const products = base + 'products';
+    const baskets = base + 'baskets';
+
+    const init = () => {
+        $http.get(products).then(res => {
+            $scope.products = res.data;
+        });
+    };
+
+    (() => {
+        init();
+    })();
+
+    $scope.createBasket = () => {
+        $scope.basket.products = $scope.products
+                                    .filter(product => product.selected)
+                                    .map(product => product.id);
+                                    
+        $http.post(baskets, $scope.basket).then(res => {
+            console.log(res);
         });
     };
 });
